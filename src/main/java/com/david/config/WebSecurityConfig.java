2b6c61a4a4e7117cc3b4b3aa68743be1aadc171a
davidchen <davidchen@bluetechnology.com.tw>
2024-03-07 22:29:56 +0800

    add DockerFile & init WebSecurity setting

--- src/main/java/com/david/config/WebSecurityConfig.java
+++ src/main/java/com/david/config/WebSecurityConfig.java
@@ -1,12 +1,28 @@
 package com.david.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity(debug = true)
 @Configuration
 public class WebSecurityConfig {
 
-    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return null;
+    }
+
 
 }
